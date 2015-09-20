c37adb44b11d18b8793d516b4a835504a9a70a21
mohitmeet2000 <mohitmeet2000@gmail.com>
2015-09-20 16:21:55 +0530

    First commit for interview prepearation code

---
+++ CodeSamples/LinearSearch/main.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <assert.h>
+
+bool search(int number1 , int arr1[], int size)
+{
+   for(int i=0;i<size;i++)
+    {
+       if(number1==arr1[i])
+           return true;           
+    }
+    return false;
+}
+
+int main()
+{   
+    bool result;
+    int number;
+    const int max_value = 10;
+    int arr[max_value];
+    int count;
+    scanf("%d %d",&count,&number);
+    assert(count < max_value);
+    for(int i=0;i<count;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    result=search(number,arr,count);
+    if(result)
+        printf("number exists");
+    else
+       printf("bhag sale");
+
+    
+    return 0;
+}    
+        
+  
+    
+
+
