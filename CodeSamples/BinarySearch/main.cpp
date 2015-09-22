bb0be07d3f03d376d633fe439d5a2906ef9e38e5
mohitmeet2000 <mohitmeet2000@gmail.com>
2015-09-22 08:11:32 +0530

    merge

--- CodeSamples/BinarySearch/main.cpp
+++ CodeSamples/BinarySearch/main.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 
-void find(int number1, int arr1[], int size)
+void find(int number1, int arr1[])
 {
 int mid;
 int beg=0;
@@ -25,7 +25,7 @@ int main()
 
 
 int arr[]={1,2,3,4,5,6,7,8,9,10};
-find(39, arr, 10);
+find(10, arr);
 }
 
 
