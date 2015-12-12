80ffa2be38d23d870c67a2a43b89d70d9c1b1456
mohitmeet2000 <mohitmeet2000@gmail.com>
2015-12-12 20:38:45 +0530

    Array Rotation By N

--- CodeSamples/ArrayRotationByN/main.cpp
+++ CodeSamples/ArrayRotationByN/main.cpp
@@ -3,6 +3,8 @@ void arrayRoatationByN(int arr1[],int size,int n)
 {
         //int Leader = arr1[size-1];
         //printf("Leader is %d",Leader);
+        if(n>size)  
+            n=n%size;
         for(int i=0;i<size-n;i++)
             {
                     int result=(size-n+i)%size;
@@ -21,7 +23,7 @@ int main()
 {
 int arr[]={7,9,3,6,2,7};
 int size=6;
-arrayRoatationByN(arr,size,3);
+arrayRoatationByN(arr,size,7);
 
 	return 0;
 }
